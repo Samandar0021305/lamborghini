7cd0aeab6a8124a9daa3c8ba7e373b599bb58db4
https://github.com/Samandar0021305 <samandar021325@gmail.com>
2022-07-23 17:32:10 +0500

    init

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let header = document.getElementById("header");
+
+let arry = [
+    "../assets/img/header-1.jpg",
+    "../assets/img/hedaer-2.jpg",
+    "../assets/img/header-3.jpg" 
+]
+
+header.style.backgroundImage = `url(${arry[0]})`
