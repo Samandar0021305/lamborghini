3d73e57d9608e2f4bd161df7251451a7d42985ef
https://github.com/Samandar0021305 <samandar021325@gmail.com>
2022-07-23 17:50:36 +0500

    init

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 let header = document.getElementById("header");
 
+let headerClick1 = document.getElementById("header_click_1")
+let headerClick2 = document.getElementById("header_click_2")
+let headerClick3 = document.getElementById("header_click_3")
+
+
+
+
 let arry = [
     "../assets/img/header-1.jpg",
     "../assets/img/hedaer-2.jpg",
     "../assets/img/header-3.jpg" 
 ]
 
-header.style.backgroundImage = `url(${arry[0]})`
+header.style.backgroundImage = `url(${arry[2]})`
+
+headerClick1.addEventListener("click",function(){
+    header.style.backgroundImage = `url(${arry[2]})`
+})
+
+headerClick2.addEventListener("click",function(){
+    header.style.backgroundImage = `url(${arry[1]})`
+})
+
+headerClick3.addEventListener("click",function(){
+    header.style.backgroundImage = `url(${arry[0]})`
+})
