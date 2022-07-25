13b63392009f6b82f69ac39c0fd2bfdd70585fc5
https://github.com/Samandar0021305 <samandar021325@gmail.com>
2022-07-25 13:23:24 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,86 @@
-let header = document.getElementById("header");
 
-let headerClick1 = document.getElementById("header_click_1")
-let headerClick2 = document.getElementById("header_click_2")
-let headerClick3 = document.getElementById("header_click_3")
+var header = document.getElementById("header");
 
+var headerClick1 = document.getElementById("header_click_1")
+var headerClick2 = document.getElementById("header_click_2")
+var headerClick3 = document.getElementById("header_click_3")
+
+
+
+var MODELS = document.getElementById("MODELS");
+var CUSTOM = document.getElementById("CUSTOM");
+var OWENRWIP  = document.getElementById("OWENRWIP");
+var MOTORSPORT = document.getElementById("MOTORSPORT");
+
+var navbar = document.getElementById("navbar")
+
+var MODELS__item = document.getElementById("MODELS__item");
+var CUSTOM__item = document.getElementById("CUSTOM__item");
+var OWENRWIP__item = document.getElementById("OWENRWIP__item");
+var MOTORSPORT__item = document.getElementById("MOTORSPORT__item");
+
+var nav = document.getElementById("nav")
+
+
+
+let nav__toogle = document.getElementById("nav__toogle")
+
+
+let navbar____tooogle = document.querySelector(".navbar____tooogle")
+
+nav__toogle.addEventListener("click",function(){
+    navbar____tooogle.classList.toggle("navbar____tooogle-block")
+})
+
+
+
+MOTORSPORT.addEventListener("mouseenter",()=>{
+    MODELS__item.classList.remove("nav-display")
+    CUSTOM__item.classList.remove("nav-display")
+    OWENRWIP__item.classList.remove("nav-display")
+    onmousDown( MOTORSPORT__item)
+})
+
+MODELS.addEventListener("mouseenter",()=>{
+    onmousDown( MODELS__item)
+    CUSTOM__item.classList.remove("nav-display")
+    OWENRWIP__item.classList.remove("nav-display")
+    MOTORSPORT__item.classList.remove("nav-display")
+})
+
+CUSTOM.addEventListener("mouseenter",()=>{
+    onmousDown( CUSTOM__item)
+    MODELS__item.classList.remove("nav-display")
+    OWENRWIP__item.classList.remove("nav-display")
+    MOTORSPORT__item.classList.remove("nav-display")
+})
+
+OWENRWIP.addEventListener("mouseenter",()=>{
+    onmousDown(OWENRWIP__item)
+    MODELS__item.classList.remove("nav-display")
+    CUSTOM__item.classList.remove("nav-display")
+    MOTORSPORT__item.classList.remove("nav-display")
+})
+
+
+
+
+
+function onmousDown(count){
+count.classList.add("nav-display")
+navbar.classList.add("nav-display")
+}
+
+
+header.addEventListener("mouseenter",()=>{
+    navbar.classList.remove("nav-display")
+    MODELS__item.classList.remove("nav-display")
+    CUSTOM__item.classList.remove("nav-display")
+    MOTORSPORT__item.classList.remove("nav-display")
+    OWENRWIP__item.classList.remove("nav-display")
+    navbar____tooogle.classList.remove
+    ("navbar____tooogle-block")
+})
 
 
 
@@ -13,23 +90,46 @@ let arry = [
     "../assets/img/header-3.jpg" 
 ]
 
-header.style.backgroundImage = `url(${arry[2]})`
+
+let header__title = document.querySelector(".header__title")
+let textArry = ["TAKE ALL YOUR SOULS TO DRIVE","IT TAKES TIME TO BECOME TIMELS","UNCLOCK ANY ROAD"]
+
+header.style.backgroundImage = `url(${arry[1]})`
 
 headerClick1.addEventListener("click",function(){
-    header.style.backgroundImage = `url(${arry[2]})`
+    header.style.backgroundImage = `url(${arry[1]})`
+    header__title.textContent = textArry[0]
 })
 
 headerClick2.addEventListener("click",function(){
-    header.style.backgroundImage = `url(${arry[1]})`
+    header.style.backgroundImage = `url(${arry[2]})`
+    header__title.textContent = textArry[1]
+    header__title.style.width = "600px"
 })
 
 headerClick3.addEventListener("click",function(){
     header.style.backgroundImage = `url(${arry[0]})`
+    header__title.textContent = textArry[2]
+
+    header__title.setAttribute("data-aos","fade-up-right")
+    header__title.setAttribute("data-aos-offset0","400")
 })
 
 
-// setInterval(setTime,3000)
+nav.addEventListener("mouseenter",function(){
+    navbar.classList.remove("nav-display")
+    MODELS__item.classList.remove("nav-display")
+    CUSTOM__item.classList.remove("nav-display")
+    MOTORSPORT__item.classList.remove("nav-display")
+    OWENRWIP__item.classList.remove("nav-display")
+})
 
-// function setTime(arry = []){
-// let normalizeeArry = arry
-// }
+
+
+
+// section pages
+
+
+
+
+
